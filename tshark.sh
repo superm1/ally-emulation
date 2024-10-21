9b5f375e3dc915e25b30e4e9af3656a44f26f170
Mario Limonciello <mario.limonciello@amd.com>
2024-10-20 23:09:47 -0500

    fix tshark.sh for 319

--- tshark.sh
+++ tshark.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tshark -r /home/supermario/src/asusmcu/ally-emulation/wireshark/firmwareupdate314.pcapng  -Y '!(usbhid.setup.ReportID == 13) && _ws.col.protocol == "USBHID" && !(_ws.col.info == "SET_IDLE Request") && !(_ws.col.info == "SET_IDLE Response") && usbhid.setup.bRequest==0x09 && usbhid.setup.wValue == 0x0300' -T fields -e frame.number -e usb.data_fragment -E separator=, | less
+tshark -r /home/supermario/src/asusmcu/ally-emulation/wireshark/firmwareupdate319.pcapng  -Y '!(usbhid.setup.ReportID == 13) && _ws.col.protocol == "USBHID" && !(_ws.col.info == "SET_IDLE Request") && !(_ws.col.info == "SET_IDLE Response") && usbhid.setup.bRequest==0x09 && usbhid.setup.wValue == 0x0300' -T fields -e frame.number -e usb.data_fragment -E separator=, | less
